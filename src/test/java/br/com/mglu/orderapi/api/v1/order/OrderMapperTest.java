48299291ec6f44aa1e6395a08297db48bec87c74
jebs <jfrossetto@gmail.com>
2024-08-23 15:03:56 -0300

    unit tests

--- src/test/java/br/com/mglu/orderapi/api/v1/order/OrderMapperTest.java
+++ src/test/java/br/com/mglu/orderapi/api/v1/order/OrderMapperTest.java
@@ -10,6 +10,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Collection;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(MockitoExtension.class)
@@ -19,11 +20,25 @@ class OrderMapperTest {
     private OrderMapper mapper;
 
     @Test
-    void should_map() {
+    void givenValidOrderQuery_whenCalltoResponse_shouldReturnOrderQueryResponse() {
         OrderQuery payload = Fixture.make(OrderQuery.builder().build());
         Collection<OrderQueryResponse> result = mapper.toResponse(List.of(payload));
         assertNotNull(result);
-
+        assertEquals(payload.getUserId(),
+                     result.stream().findFirst().map(OrderQueryResponse::getUserId).orElse(null));
+        assertEquals(payload.getOrderId(),
+                result.stream()
+                        .flatMap(orderQueryResponse -> orderQueryResponse.getOrders().stream())
+                        .findFirst()
+                        .map(OrderResponse::getOrderId)
+                        .orElse(null));
+        assertEquals(payload.getProductId(),
+                result.stream()
+                        .flatMap(orderQueryResponse -> orderQueryResponse.getOrders().stream())
+                        .flatMap(orderQueryResponse -> orderQueryResponse.getProducts().stream())
+                        .findFirst()
+                        .map(OrderProductResponse::getProductId)
+                        .orElse(null));
     }
 
 }
--- src/test/java/br/com/mglu/orderapi/order/OrderServiceTest.java
+++ src/test/java/br/com/mglu/orderapi/order/OrderServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.mglu.orderapi.order;
 
 import br.com.mglu.orderapi.Fixture;
+import br.com.mglu.orderapi.api.v1.order.OrderFilter;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -10,6 +11,7 @@ import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,12 +27,25 @@ class OrderServiceTest {
     private OrderService service;
 
     @Test
-    void should_findById() {
+    void givenValidOrderId_whenCallFindById_shouldReturnOrderQuery() {
         OrderQuery orderQuery = Fixture.make(OrderQuery.builder().build());
         when(repository.findById(1)).thenReturn(Flux.just(orderQuery));
         StepVerifier.create(service.findById(1))
                 .consumeNextWith(result -> {
                     assertNotNull(result);
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void givenValidOrderFilter_whenCallFindByFilters_shouldReturnOrderQuery() {
+        OrderQuery orderQuery = Fixture.make(OrderQuery.builder().build());
+        OrderFilter filter = new OrderFilter(1, null, null, null);
+        when(customRepository.findByFilters(filter)).thenReturn(Flux.just(orderQuery));
+        StepVerifier.create(service.findByFilters(filter))
+                .consumeNextWith(result -> {
+                    assertNotNull(result);
+                    verify(customRepository).findByFilters(filter);
                 })
                 .verifyComplete();
     }
